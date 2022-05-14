25faa373f3162e92fbd0e03a9b9a017188215f54
Mauro <MauroMarchesan88@gmail.com>
2022-05-14 16:22:13 -0300

    Componente MenuInferior coberto

---
+++ src/Testes/Footer.test.js
@@ -0,0 +1,27 @@
+import { screen } from '@testing-library/react';
+// import userEvent from '@testing-library/user-event';
+import React from 'react';
+import App from '../App';
+import renderWithRouter from './Helpers/renderWithRouter';
+
+describe('Teste o componente <Login.js>',
+  () => {
+    // const testId = 'data-testid';
+    const drinksId = 'drinks-bottom-btn';
+    const exploreId = 'explore-bottom-btn';
+    const foodsId = 'food-bottom-btn';
+
+    it('Verifica os data-testids dos elementos', async () => {
+      renderWithRouter(<App />, '/foods');
+
+      const footer = await screen.findByTestId('footer');
+      const drinksBtn = await screen.findByTestId(drinksId);
+      const exploreBtn = await screen.findByTestId(exploreId);
+      const foodsBtn = await screen.findByTestId(foodsId);
+
+      expect(footer).toBeInTheDocument();
+      expect(drinksBtn).toBeInTheDocument();
+      expect(exploreBtn).toBeInTheDocument();
+      expect(foodsBtn).toBeInTheDocument();
+    });
+  });
